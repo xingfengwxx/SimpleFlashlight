9a713d14a2ee58f42256f01012f08c49b9cd8941
xingfengwxx <1099420259@qq.com>
2019-05-17 14:47:31 +0800

    Initial commit

---
+++ app/src/main/java/com/wangxingxing/flashlight/BaseApplication.kt
@@ -0,0 +1,11 @@
+package com.wangxingxing.flashlight
+
+import android.app.Application
+
+class BaseApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+}
